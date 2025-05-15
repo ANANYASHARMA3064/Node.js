a282511b7a7630f6703ae756988b441d5ce776ce
Ananya Sharma <ananyasharma3064@gmail.com>
2025-05-14 22:53:16 -0400

    learned about modules

--- 2-global.js
+++ 2-global.js
@@ -5,6 +5,3 @@
 //module -info about current module 
 //process -info about env where the program  being executed 
 console.log(__dirname); 
-setInterval(()=>{
-    console.log('hello world')
-},1000)
--- app.js
+++ app.js
@@ -1,9 +1,8 @@
- const amount = 12 
- if(amount<10){
-    console.log('small')
- }
- else {
-    console.log('large number')
-}
-console.log(`hey its my first node app`)
- 
+// modules
+const names = require('./4-name')
+console.log(names)
+//module happens to be an object apparently 
+const sayHi =require('./5-utils')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter) 
