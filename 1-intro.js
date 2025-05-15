1a1fd86ca089157aa10a11e85eadffb6d1a6e17f
Ananya Sharma <ananyasharma3064@gmail.com>
2025-05-14 21:55:23 -0400

    ✨ initial commit: starting my Node.js journey

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+//Global-NO WINDOW !!!
+//__dirame - path to current directory 
+// __filename - file name
+// require -function to use modules (CommonJS)
+//module -info about current module 
+//process -info about env where the program  being executed 
+console.log(__dirname); 
+setInterval(()=>{
+    console.log('hello world')
+},1000)
