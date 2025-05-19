fd5b3c02833d61b6aa4933f816a499b56522e316
Ananya Sharma <ananyasharma3064@gmail.com>
2025-05-19 16:17:01 -0400

    learned fs-module sync

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -5,6 +5,6 @@ console.log(first,second)
 //The fs module (short for File System) in Node.js lets you interact with the file system — like reading/writing/editing files and folders.
 writeFileSync(
     './content/result-sync.txt',
-    `Here is the result :${first},${second}`,{flag:'a'}
+    `Here is the result :${first},${second}`,{flag:'a '}
 
 )
