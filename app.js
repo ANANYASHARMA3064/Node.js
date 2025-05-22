d0f510002750955af07039e9ed354b3ec714a15e
Ananya Sharma <ananyasharma3064@gmail.com>
2025-05-21 23:09:28 -0400

    learned fs-module async

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const { readFileSync,writeFileSync }=require('fs')
-const first= readFileSync('./content/first.txt','utf-8')
-const second= readFileSync('./content/second.txt','utf-8') 
-console.log(first,second)
-//The fs module (short for File System) in Node.js lets you interact with the file system — like reading/writing/editing files and folders.
-writeFileSync(
-    './content/result-sync.txt',
-    `Here is the result :${first},${second}`,{flag:'a'}
-
-)
+const {readFile,writeFile} = require('fs')
+// a synchronous method requires a callback
+readFile('./content/first.txt','utf8',(err,result)=>{
+    if (err){
+        console.log(err)
+        return
+    }
+    console.log(result)
+}) 
+// in node the first argument of readfile is always the error and the second one is always the result 
