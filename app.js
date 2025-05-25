862864b3658e0966d919c81f682b33aa110eb6b0
Ananya Sharma <ananyasharma3064@gmail.com>
2025-05-24 23:27:07 -0400

    learned abiut the http module

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.write('Welcome to our home page');
-  res.end();
+  if(req.url==='/'){
+    res.end('welcome to our webpage')
+  }
+  if(req.usj ==='/about'){
+    res.end('here is our short history ')
+    return
+  }
+  res.end(`
+    <h1>Oops!</h1>
+    `)
 });
-
-server.listen(5001);
+server.listen(5001, () => {
+  console.log('✅ Server is running on http://localhost:5001');
+});
