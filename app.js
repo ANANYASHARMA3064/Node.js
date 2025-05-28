d94ca87b729a2fafd90e25be79c60ba5670516c4
Ananya Sharma <ananyasharma3064@gmail.com>
2025-05-27 22:39:44 -0400

    started with npm

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  if(req.url==='/'){
-    res.end('welcome to our webpage')
-  }
-  if(req.usj ==='/about'){
-    res.end('here is our short history ')
-    return
-  }
-  res.end(`
-    <h1>Oops!</h1>
-    `)
-});
-server.listen(5001, () => {
-  console.log('✅ Server is running on http://localhost:5001');
-});
+//npm --global command,comes with node 
+//npm --version
+//local dependency -use it only in this particular project 
+// npm i <packagename>
+// global dependency -use it in any project 
+// npm install -g<oackagename>
+// sudo npm install -g<packagename>(mac)
+
+//basically no idea what I am doing 
