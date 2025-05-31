d72f0ba31d5fc782357998eefde0e2d39cb634c9
Ananya Sharma <ananyasharma3064@gmail.com>
2025-05-30 21:01:31 -0400

    learned about packages

--- app.js
+++ app.js
@@ -5,5 +5,10 @@
 // global dependency -use it in any project 
 // npm install -g<oackagename>
 // sudo npm install -g<packagename>(mac)
+const _ =require('lodash')
+const items =[1,[2,3],4]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
+
 
 
