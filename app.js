8db5689e4d22998059a28c8a93533f4e4475ea53
Ananya Sharma <ananyasharma3064@gmail.com>
2025-06-18 22:43:25 -0400

    revision-promises

--- app.js
+++ app.js
@@ -15,5 +15,16 @@ const getText =(path)=>{
 getText('./content/first.txt')
     .then((result)=>console.log(result))
     .catch((err)=>console.log(err))
+const start = async()=>{
+    try{
+            const first = await getText('./content/first.txt')
+            const second= await getText('./content/second.txt')
+            console.log(first,second)
 
-#30JANNtgthrrthyh fgghhh
+
+    }
+    catch(error){
+        console.log()
+    }
+    
+}
