cd113cbbf81d22434e74f654b6095783eb5a2320
Ananya Sharma <ananyasharma3064@gmail.com>
2025-06-19 21:06:37 -0400

    learned to use promises with await

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const {readFile} = require(  'fs')
-
-const getText =(path)=>{
-    return new Promise((resolve,reject)=>{
-        readFile('./content/first.txt','utf8',(err,data)=>{
-            if (err){
-                reject(err)
-    }
-            else{
-                resolve(data) 
-    }
-})
-    })
-}
-getText('./content/first.txt')
-    .then((result)=>console.log(result))
-    .catch((err)=>console.log(err))
+const {readFile,writeFile, read} = require(  'fs')
+const util=require('util');
+const readFilePromise=util.promisify(readFile)
+const writeFilePromise =util.promisify(writeFile)
+// const getText =(path)=>{
+//     return new Promise((resolve,reject)=>{
+//         readFile(path,'utf8',(err,data)=>{
+//             if (err){
+//                 reject(err)
+//     }
+//             else{
+//                 resolve(data) 
+//     }
+// })
+//     })
+// }
+// getText('./content/first.txt')
+//     .then((result)=>console.log(result))
+//     .catch((err)=>console.log(err))
 const start = async()=>{
     try{
-            const first = await getText('./content/first.txt')
-            const second= await getText('./content/second.txt')
+            const first = await readFilePromise('./content/first.txt')
+            const second= await readFilePromise('./content/second.txt')
+            await writeFilePromise('./content/something.txt',`this is awesome ${first},${second}`) 
             console.log(first,second)
 
 
@@ -28,3 +31,4 @@ const start = async()=>{
     }
     
 }
+start()
