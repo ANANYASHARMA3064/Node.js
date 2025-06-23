4783e50168666f008f2d0b3ba1bf560051aa234c
Ananya Sharma <ananyasharma3064@gmail.com>
2025-06-22 23:03:11 -0400

    .promisify

---
+++ 13-promises.js
@@ -0,0 +1,34 @@
+const {readFile,writeFile, read} = require(  'fs').promises
+const util=require('util');
+const readFilePromise=util.promisify(readFile)
+const writeFilePromise =util.promisify(writeFile)
+// const getText =(path)=>{
+//     return new Promise((resolve,reject)=>{
+//         readFile(path,'utf8',(err,data)=>{
+//             if (err){
+//                 reject(err)
+//     }
+//             else{
+//                 resolve(data) 
+//     }
+// })
+//     })
+// }
+// getText('./content/first.txt')
+//     .then((result)=>console.log(result))
+//     .catch((err)=>console.log(err))
+const start = async()=>{
+    try{
+            const first = await readFile('./content/first.txt')
+            const second= await readFile('./content/second.txt')
+            await writeFilePromise('./content/something.txt',`this is awesome ${first},${second}`,{flag:'a'}) 
+            console.log(first,second)
+
+
+    }
+    catch(error){
+        console.log()
+    }
+    
+}
+start() 
--- app.js
+++ app.js
@@ -1,34 +1,13 @@
-const {readFile,writeFile, read} = require(  'fs').promises
-const util=require('util');
-const readFilePromise=util.promisify(readFile)
-const writeFilePromise =util.promisify(writeFile)
-// const getText =(path)=>{
-//     return new Promise((resolve,reject)=>{
-//         readFile(path,'utf8',(err,data)=>{
-//             if (err){
-//                 reject(err)
-//     }
-//             else{
-//                 resolve(data) 
-//     }
-// })
-//     })
-// }
-// getText('./content/first.txt')
-//     .then((result)=>console.log(result))
-//     .catch((err)=>console.log(err))
-const start = async()=>{
-    try{
-            const first = await readFile('./content/first.txt')
-            const second= await readFile('./content/second.txt')
-            await writeFilePromise('./content/something.txt',`this is awesome ${first},${second}`,{flag:'a'}) 
-            console.log(first,second)
+const EventEmitter =require('events')
+const coustomEmitter =new EventEmitter()
+coustomEmitter.on('response',()=>{
+    console.log(`data recieved`)
 
+})
+customEmmiter.emit('response',()=>{
+    console.log(`some other logic here`)
+})
+customEmmiter.emitter('response')
+ 
 
-    }
-    catch(error){
-        console.log()
-    }
-    
-}
-start()
+ 
