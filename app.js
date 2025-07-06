dcb7dd8a0546d383d0ecebef8a929aeb4e05a690
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-05 23:38:20 -0400

    readfilestream

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const http = require('http')
+// const http = require('http')
 
-// const server = http.createServer((req, res) => {
+// // const server = http.createServer((req, res) => {
+// //   res.end('Welcome')
+// // })
+
+// // Using Event Emitter API
+// const server = http.createServer()
+// // emits request event
+// // subcribe to it / listen for it / respond to it
+// server.on('request', (req, res) => {
 //   res.end('Welcome')
 // })
 
-// Using Event Emitter API
-const server = http.createServer()
-// emits request event
-// subcribe to it / listen for it / respond to it
-server.on('request', (req, res) => {
-  res.end('Welcome')
+// server.listen(8080)
+const {createReadStream} =require('fs');
+const stream = createReadStream('./content/big.txt');
+stream.on('data',(result)=>{
+  console.log(result)
 })
-
-server.listen(8080)
