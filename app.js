731070da0ef3a6bbdd0823f06eaf8067ef54b37d
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-07 23:07:30 -0400

    learned additional info about streams

--- app.js
+++ app.js
@@ -14,7 +14,11 @@
 
 // server.listen(8080)
 const {createReadStream} =require('fs');
-const stream = createReadStream('./content/big.txt');
+const stream = createReadStream('../content/big.txt',{
+  highWaterMark:9000,
+  encoding: 'utf-8'
+});
 stream.on('data',(result)=>{
   console.log(result )
 })
+stream.on('error',(err)=>console.log(err ))
