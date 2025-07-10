d58ec9e0ea4af76659860f2bbfd233994ee2d9d6
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-10 19:10:47 -0400

    pipesss

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-// const http = require('http')
+var http = require('http')
+var fs = require('fs')
 
-// // const server = http.createServer((req, res) => {
-// //   res.end('Welcome')
-// // })
-
-// // Using Event Emitter API
-// const server = http.createServer()
-// // emits request event
-// // subcribe to it / listen for it / respond to it
-// server.on('request', (req, res) => {
-//   res.end('Welcome')
-// })
-
-// server.listen(8080)
-const {createReadStream} =require('fs');
-const stream = createReadStream('../content/big.txt',{
-  highWaterMark:9000,
-  encoding: 'utf-8'
-});
-stream.on('data',(result)=>{
-  console.log(result )
-})
-stream.on('error',(err)=>console.log(err ))
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
+    fileStream.on('open', () => {
+      fileStream.pipe(res)
+    })
+    fileStream.on('error', (err) => {
+      res.end(err)
+    })
+  })
+  .listen(5000)
