e68d59abfafcb0dbe2e8ebe47333f832b17ed161
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-13 20:25:48 -0400

    https header

--- node-express-course/02-express-tutorial/new-app.js
+++ node-express-course/02-express-tutorial/new-app.js
@@ -1,6 +1,8 @@
 const http =require('http')
 const server =http.createServer((req,res)=>{
     console.log('user hit the server')
-    res.end('hello B words!')
+    res.writeHead(200,{'content-type':'text/html'})
+    res.write('<h1>hello B words!<h1>')
+    res.end()
 })
 server.listen(7080)
