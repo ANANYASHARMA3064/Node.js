7b9bd60e191569ab90e7e2fc1c1456cd675862bb
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-14 23:32:49 -0400

    learned https headers

--- node-express-course/02-express-tutorial/new-app.js
+++ node-express-course/02-express-tutorial/new-app.js
@@ -1,5 +1,7 @@
 const http =require('http')
 const server =http.createServer((req,res)=>{
+    console.log(req.method)
+    console.log(req.url)
     console.log('user hit the server')
     res.writeHead(200,{'content-type':'text/html'})
     res.write('<h1>hello B words!<h1>')
