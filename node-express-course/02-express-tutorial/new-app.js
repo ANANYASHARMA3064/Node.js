b6dfbcb42bf60d2c6d2d5f57644fe5ace646bcb6
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-15 23:31:30 -0400

    http request

--- node-express-course/02-express-tutorial/new-app.js
+++ node-express-course/02-express-tutorial/new-app.js
@@ -2,9 +2,26 @@ const http =require('http')
 const server =http.createServer((req,res)=>{
     console.log(req.method)
     console.log(req.url)
-    console.log('user hit the server')
+    const url=req.url;
+    if (url==='/'){
+    // console.log('user hit the server')
     res.writeHead(200,{'content-type':'text/html'})
     res.write('<h1>hello B words!<h1>')
     res.end()
+    }
+    //about page
+    else if(url==='/about'){
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write('<h1>ABOUT ME : I am me<h1>')
+        res.end()
+
+    }
+    else{
+        res.writeHead(404,{'content-type':'text/html'})
+        res.write('<h1>404 :((((((<h1>')
+        res.end()
+
+    }
+    
 })
 server.listen(7080)
