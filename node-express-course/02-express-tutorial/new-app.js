b5de287ecd7dcf725332132f873e6c1adbeeeeba
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-18 19:42:53 -0400

    extra express basics

--- node-express-course/02-express-tutorial/new-app.js
+++ node-express-course/02-express-tutorial/new-app.js
@@ -2,8 +2,16 @@ const express =require('express');
 const app = express()
 app.get('/',(req,res)=>{
     console.log('hello')
-    res.send('Home page')
+    res.status(200).send('Home page')
 })
+app.get('/about',(req,res)=>{
+    console.log('hello')
+    res.status(200).send('About me ')
+})
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>resource not found <h1>')
+})
+
 app.listen(8090,()=>{
     console.log('the server is listening on port 5000') 
 })
