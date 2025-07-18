8343bed4b88f472dc2be427742fc9aef60fac8a9
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-17 20:02:43 -0400

    https example

--- node-express-course/02-express-tutorial/new-app.js
+++ node-express-course/02-express-tutorial/new-app.js
@@ -1,6 +1,9 @@
 const http =require('http')
 const {readFileSync}=require('fs')
-const homepage =readFileSync('./index.html')
+const homepage =readFileSync('./navbar-app/index.html')
+const homestyles =readFileSync('./navbar-app/styles.css')
+const homeimage =readFileSync('./navbar-app/logo.svg')
+const homelogic =readFileSync('./navbar-app/browser-app.js')
  
 const server =http.createServer((req,res)=>{
     console.log(req.method)
@@ -16,6 +19,27 @@ const server =http.createServer((req,res)=>{
     else if(url==='/about'){
         res.writeHead(200,{'content-type':'text/html'})
         res.write('<h1>ABOUT ME : I am me<h1>')
+        res.end()
+
+    }
+    // styles
+    else if(url==='/styles.css'){
+        res.writeHead(200,{'content-type':'text/css'})
+        res.write(homestyles)
+        res.end()
+
+    }
+    // image/logo
+    else if(url==='/logo.svg'){
+        res.writeHead(200,{'content-type':'image/svg+xml'})
+        res.write(homeimage )
+        res.end()
+
+    }
+     // logic
+    else if(url==='/browser-app.js '){
+        res.writeHead(200,{'content-type':'text/javascript'})
+        res.write(homeimage )
         res.end()
 
     }
