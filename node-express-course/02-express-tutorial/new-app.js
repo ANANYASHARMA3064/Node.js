b79617c54d556243cd5d9160eb1e8e73afef04ba
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-18 20:28:58 -0400

    learned about express.static()

--- node-express-course/02-express-tutorial/new-app.js
+++ node-express-course/02-express-tutorial/new-app.js
@@ -1,17 +1,15 @@
 const express =require('express');
-const app = express()
+const path =require('path'); 
+const app =express();
+
+app.use(express.static('./public'))
+ 
 app.get('/',(req,res)=>{
-    console.log('hello')
-    res.status(200).send('Home page')
-})
-app.get('/about',(req,res)=>{
-    console.log('hello')
-    res.status(200).send('About me ')
+    res.sendFile((path.resolve(__dirname,'./navbar-app/index.html')))
 })
 app.all('*',(req,res)=>{
-    res.status(404).send('<h1>resource not found <h1>')
+    res.status(404).send('resource not found')
 })
-
 app.listen(8090,()=>{
-    console.log('the server is listening on port 5000') 
+    console.log('server is listening on port 8090')
 })
