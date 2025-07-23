80ea9b1f77584edea6c8d84993fb3fa4bb3c17c7
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-22 23:32:08 -0400

    json basics

--- node-express-course/02-express-tutorial/new-app.js
+++ node-express-course/02-express-tutorial/new-app.js
@@ -1,9 +1,10 @@
 const express=require('express')
 const app =express()
-
+const {products}=require('./data ')
 app.get('/',(req,res)=>{
-    res.json([{name:'john'},{name:susan}])
+    res.json(products)
 })
 app.listen(8090,()=>{
     console.log('the server is listening on port 8090')
 })
+//
