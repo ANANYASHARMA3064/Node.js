4ccaea4e43a9fd31b3d6e46fc31253d06fbaa52f
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-22 23:50:04 -0400

    params,query,string-setup

--- node-express-course/02-express-tutorial/new-app.js
+++ node-express-course/02-express-tutorial/new-app.js
@@ -2,9 +2,15 @@ const express=require('express')
 const app =express()
 const {products}=require('./data ')
 app.get('/',(req,res)=>{
-    res.json(products)
+    res.send('<h1>Home page</h1><a href="/api/products">products</a>' )
+})
+app.get('/api/products',(req,res)=>{
+    const newProducts = products.map((product)=>{
+        const {id,name,image}=product;
+        return {id,name,image}
+    })
+    res.json(newProducts)
 })
 app.listen(8090,()=>{
     console.log('the server is listening on port 8090')
 })
-//
