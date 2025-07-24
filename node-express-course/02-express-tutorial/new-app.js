9c6f99ba7d31197970dc775a59e6c7bdb120ca20
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-23 21:48:55 -0400

    route params

--- node-express-course/02-express-tutorial/new-app.js
+++ node-express-course/02-express-tutorial/new-app.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const app =express()
-const {products}=require('./data ')
+const {products}=require('./data.js')
 app.get('/',(req,res)=>{
     res.send('<h1>Home page</h1><a href="/api/products">products</a>' )
 })
@@ -10,6 +10,16 @@ app.get('/api/products',(req,res)=>{
         return {id,name,image}
     })
     res.json(newProducts)
+})
+app.get('/api/products/:productID',(req,res)=>{
+    console.log(req)
+    console.log(req.params)
+    const{productID}=req.params; 
+    const singleProduct = products.find((products)=>products.id === Number(productID))
+    if (!singleProduct){
+        return res.status(404).send('product does not hjjh exist')
+    }
+    res.json(singleProduct)
 })
 app.listen(8090,()=>{
     console.log('the server is listening on port 8090')
