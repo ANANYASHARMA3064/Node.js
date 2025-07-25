a7382799b80e429ba248fff9c7080092208073de
Ananya Sharma <ananyasharma3064@gmail.com>
2025-07-24 20:24:55 -0400

    extra info about route params

--- node-express-course/02-express-tutorial/new-app.js
+++ node-express-course/02-express-tutorial/new-app.js
@@ -17,9 +17,13 @@ app.get('/api/products/:productID',(req,res)=>{
     const{productID}=req.params; 
     const singleProduct = products.find((products)=>products.id === Number(productID))
     if (!singleProduct){
-        return res.status(404).send('product does not hjjh exist')
+        return res.status(404).send('product does not  exist')
     }
     res.json(singleProduct)
+})
+app.get('/api/products/:productID/reviews/:reviewID',(req,res)=>{
+    console.log(req.params)
+    res.send('hello world ')
 })
 app.listen(8090,()=>{
     console.log('the server is listening on port 8090')
