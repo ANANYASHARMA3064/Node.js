b42b41502ba53e378b078bc774408d2fdfee9026
Ananya Sharma <ananyasharma3064@gmail.com>
2025-08-08 20:19:33 -0400

    POST -part-1🥲

--- node-express-course/02-express-tutorial/new-app.js
+++ node-express-course/02-express-tutorial/new-app.js
@@ -2,6 +2,8 @@ const express =require('express')
 const app = express()
 const logger =require('./logger')
 let {people}=require('./data')
+//static assets
+app.use(express.static('./methods-public'))
 app.get('/api/people',(req,res)=>{
     res.status(200).json({success:true,data:people })
 })
