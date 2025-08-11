84ba8ed0eac2dc460830c2d8d29e75d52373cec5
Ananya Sharma <ananyasharma3064@gmail.com>
2025-08-10 20:16:01 -0400

    POST-PART2 :)

--- node-express-course/02-express-tutorial/new-app.js
+++ node-express-course/02-express-tutorial/new-app.js
@@ -4,8 +4,19 @@ const logger =require('./logger')
 let {people}=require('./data')
 //static assets
 app.use(express.static('./methods-public'))
+// parse from data
+app.use(express.urlencoded({extended:false}))
 app.get('/api/people',(req,res)=>{
     res.status(200).json({success:true,data:people })
+})
+app.post('/login',(req,res )=>{
+    const {name}=req.body
+    if (name){
+        return res.status(200).send(`welcome ${name}`)
+    }
+    res.status(401).send('Please provide creadentials ')
+    console.log(req.body)
+    res.send('POST')
 })
 app.listen(8090,()=>{
     console.log('server is listening on port 8090:')
