47419a462fb55793c635a9bc98310f42a0eef54a
Ananya Sharma <ananyasharma3064@gmail.com>
2025-08-22 20:52:51 -0400

    FINISHEDgit add .

--- node-express-course/02-express-tutorial/Controllers/People.js
+++ node-express-course/02-express-tutorial/Controllers/People.js
@@ -1,25 +1,19 @@
-const express = require('express');
- const router = express.Router();
- 
-
- let {people}=require('../data')
-
-router.get('/',(req,res)=>{
-    res.status(200).json({success:true,data:people })
-})
-router.post('/',(req,res)=>{ const {name}=req.body
+const getPeople = (req,res)=>{
+    res.status(200).json({sucess:true,data:people})
+}
+const createPerson = (req,res)=>{ const {name}=req.body
     if (!name){
         return res.status(400).json({success:false,msg:'please provide name value'})
     }
     res.status(201).json('Success') 
-})
-router.post('/postman',(req,res)=>{
+}
+const createPersonPostman =(req,res)=>{
     const {name}=req.body
      if (!name){
         return res.status(400).json({success:false,msg:'please provide name value'})
     } 
     res.status(201).json({success:true,data:[...people,name]})
-})
+} 
 router.put('/:id',(req,res)=>{
     const {id}=req.params
     const {name}=req.body 
@@ -38,13 +32,3 @@ router.put('/:id',(req,res)=>{
     res.status(200).json({success:true,data:newPeople})
 
 })
-router.delete('/ :id',(req,res)=>{
-    const person =people.find((person)=>person.id===Number(req.params.id ))
-     if (!person){
-        return res.status(400).json({success:false,msg:'no such id  '})
-    } 
-    const newPeople =people.filter((person)=>{person.id !== Number(req.params.id)})
-    return res.status(200).json({success:true,data:newPeople })
-})
-module.exports = router 
- 
